d9a6e43f3470c4a658d8edfa1948d611ce224272
shaneboulden <shane.boulden@gmail.com>
2024-01-17 10:23:20 +1000

    Minor updates

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 Shane Boulden (shane.boulden@gmail.com)
+Copyright © 2023 Shane Boulden (sboulden@redhat.com)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
